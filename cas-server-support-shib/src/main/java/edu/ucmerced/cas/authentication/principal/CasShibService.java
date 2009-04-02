f46851a12c5c958afd3822dba7ecdd51e5a8fe4e
bkoehm <bkoehm@e384b71a-1fa8-11de-a108-cd2f117ce590>
2009-04-02 23:55:18 +0000

    An extension to the WebapplicationService interface that adds the appNameOrPasscode attribute to CASShib services.
    git-svn-id: https://casshib.googlecode.com/svn/trunk@12 e384b71a-1fa8-11de-a108-cd2f117ce590

---
+++ cas-server-support-shib/src/main/java/edu/ucmerced/cas/authentication/principal/CasShibService.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2007 The JA-SIG Collaborative. All rights reserved. See license
+ * distributed with this file and available online at
+ * http://www.uportal.org/license.html
+ */
+package edu.ucmerced.cas.authentication.principal;
+
+import org.jasig.cas.authentication.principal.WebApplicationService;
+
+/**
+ * An extension to the web application service interface that allows for the
+ * configuration of an application name or a passcode.  This is used for
+ * additional service authentication during CASShib requests.
+ *
+ * @author Brian Koehmstedt
+ */
+public interface CasShibService extends WebApplicationService
+{
+  public String getAppNameOrPasscode();
+}
