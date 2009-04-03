7cc55b23c642fa9b6fb7f3744d2adc13ce19bbb9
bkoehm <bkoehm@e384b71a-1fa8-11de-a108-cd2f117ce590>
2009-04-03 01:09:30 +0000

    A modified cookie generator that will set the cookie name and cookie path based on the registered app name.  This is needed so CAS differentiates authentication credentials for different Shibboleth services.
    git-svn-id: https://casshib.googlecode.com/svn/trunk@21 e384b71a-1fa8-11de-a108-cd2f117ce590

---
+++ cas-server-support-shib/src/main/java/edu/ucmerced/cas/web/support/CasShibCookieRetrievingCookieGenerator.java
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2007 The JA-SIG Collaborative. All rights reserved. See license
+ * distributed with this file and available online at
+ * http://www.uportal.org/license.html
+ */
+package edu.ucmerced.cas.web.support;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jasig.cas.authentication.principal.RememberMeCredentials;
+import org.jasig.cas.services.UnauthorizedServiceException;
+import org.springframework.util.StringUtils;
+import org.springframework.web.util.CookieGenerator;
+import edu.ucmerced.cas.services.CasShibServiceRegistrar;
+
+import org.inspektr.common.ioc.annotation.NotNull;
+import java.net.URLEncoder;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Extends CookieGenerator to allow you to retrieve a value from a request.
+ * 
+ * <p/>
+ * 
+ * Also has support for RememberMe Services
+ * 
+ * <p/>
+ * 
+ * This class has been modified from the original to issue cookies with
+ * differing names and cookie paths based on the request URI conventions for
+ * CasShib. This is necessary so that CAS doesn't cache authentication
+ * credentials for different Shibboleth services.
+ * 
+ * @see AbstractShibEnabledArgumentExtractor for request URI convention.
+ * 
+ * <p/>
+ * 
+ * Cookies will be issued as such: CASTGC-<appName> with a cookie path of /<contextPath>/<appName>.
+ * 
+ * <p/>
+ * 
+ * Since the cookie name and cookie path can change upon any given request, the
+ * Spring CookieGenerator must be instantiated on a per-request basis rather
+ * than instantiated just once upon application initialization.
+ * 
+ * <p/>
+ * 
+ * Derived from: org.jasig.cas.web.support.CookieRetrievingCookieGenerator
+ * 
+ * @author Scott Battaglia
+ * @author modified by Brian Koehmstedt
+ * 
+ */
+public class CasShibCookieRetrievingCookieGenerator {
+
+    /**
+     * The maximum age the cookie should be remembered for. The default is three
+     * months (7889231 in seconds, according to Google)
+     */
+    private int rememberMeMaxAge = 7889231;
+
+    /** should cookie be secure (https) only? */
+    private Boolean cookieSecure;
+
+    /** cookie max age */
+    private Integer cookieMaxAge;
+
+    /** cookie name prefix */
+    @NotNull
+    private String cookieNamePrefix;
+
+    /** registrations that contain service ID to passcode mappings */
+    @NotNull
+    private CasShibServiceRegistrar shibServiceRegistrar;
+
+    /** override createCookie to make it public instead of protected */
+    protected static class LocalCookieGenerator extends CookieGenerator {
+        @Override
+        @SuppressWarnings("PMD.UselessOverridingMethod")
+        /** public instead of protected */
+        public Cookie createCookie(String cookieValue) {
+            return super.createCookie(cookieValue);
+        }
+    }
+
+    public void addCookie(final HttpServletRequest request,
+        final HttpServletResponse response, final String cookieValue) {
+        // instantiate a new CookieGenerator upon every request because the
+        // cookie name and path are possibly going to be different for each
+        // request
+        LocalCookieGenerator cookieGenerator = newCookieGeneratorInstance(request);
+
+        if (!StringUtils.hasText(request
+            .getParameter(RememberMeCredentials.REQUEST_PARAMETER_REMEMBER_ME))) {
+            cookieGenerator.addCookie(response, cookieValue);
+        } else {
+            final Cookie cookie = cookieGenerator.createCookie(cookieValue);
+            cookie.setMaxAge(this.rememberMeMaxAge);
+            if (cookieGenerator.isCookieSecure()) {
+                cookie.setSecure(true);
+            }
+            response.addCookie(cookie);
+        }
+    }
+
+    public String retrieveCookieValue(final HttpServletRequest request) {
+        String appName = CasShibUtil.getAppNameFromRequestURI(request,
+            shibServiceRegistrar);
+        if (appName == null) {
+            // can't determine appName so can't determine cookie to retrieve
+            return null;
+        }
+
+        final Cookie cookie = org.springframework.web.util.WebUtils.getCookie(
+            request, getFullCookieName(appName));
+
+        return cookie == null ? null : cookie.getValue();
+    }
+
+    public void setRememberMeMaxAge(final int maxAge) {
+        this.rememberMeMaxAge = maxAge;
+    }
+
+    public void setCookieSecure(final Boolean cookieSecure) {
+        this.cookieSecure = cookieSecure;
+    }
+
+    public void setCookieMaxAge(final Integer cookieMaxAge) {
+        this.cookieMaxAge = cookieMaxAge;
+    }
+
+    public void setCookieNamePrefix(final String cookieNamePrefix) {
+        this.cookieNamePrefix = cookieNamePrefix;
+    }
+
+    protected LocalCookieGenerator newCookieGeneratorInstance(
+        final HttpServletRequest request) {
+        String appName = CasShibUtil.getAppNameFromRequestURI(request,
+            shibServiceRegistrar);
+        if (appName == null) {
+            throw new UnauthorizedServiceException(
+                "Can't issue cookies because application name could not be determined.");
+        }
+
+        LocalCookieGenerator cookieGenerator = new LocalCookieGenerator();
+        if (cookieSecure != null)
+            cookieGenerator.setCookieSecure(cookieSecure.booleanValue());
+        if (cookieMaxAge != null)
+            cookieGenerator.setCookieMaxAge(cookieMaxAge.intValue());
+
+        // cookie name is cookieNamePrefix + "-" + escaped(appName)
+        cookieGenerator.setCookieName(getFullCookieName(appName));
+
+        // determine the cookie path based on the requestURI
+        String cookiePath = (request.getContextPath() != null ? request
+            .getContextPath() : "")
+            + "/shib/" + appName;
+
+        cookieGenerator.setCookiePath(cookiePath);
+
+        return (cookieGenerator);
+    }
+
+    public void removeCookie(final HttpServletRequest request,
+        final HttpServletResponse response) {
+        LocalCookieGenerator cookieGenerator = newCookieGeneratorInstance(request);
+        cookieGenerator.removeCookie(response);
+    }
+
+    public void setCasShibServiceRegistrar(
+        final CasShibServiceRegistrar shibServiceRegistrar) {
+        this.shibServiceRegistrar = shibServiceRegistrar;
+    }
+
+    protected String getFullCookieName(String appName) {
+        try {
+            return (cookieNamePrefix + "-" + URLEncoder
+                .encode(appName, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            // shouldn't ever happen, UTF-8 is a supported encoding
+            throw new RuntimeException(e);
+        }
+    }
+}
