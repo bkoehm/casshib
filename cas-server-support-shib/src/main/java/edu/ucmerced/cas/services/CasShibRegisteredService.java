1e72e0ceaae08cce74fefdf8fb7a524aae7ed9e0
bkoehm <bkoehm@e384b71a-1fa8-11de-a108-cd2f117ce590>
2009-04-03 00:09:21 +0000

    An extension to the registered service interface to allow a service to have a passcode.
    git-svn-id: https://casshib.googlecode.com/svn/trunk@15 e384b71a-1fa8-11de-a108-cd2f117ce590

---
+++ cas-server-support-shib/src/main/java/edu/ucmerced/cas/services/CasShibRegisteredService.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2007 The JA-SIG Collaborative. All rights reserved. See license
+ * distributed with this file and available online at
+ * http://www.ja-sig.org/products/cas/overview/license/
+ */
+package edu.ucmerced.cas.services;
+
+import org.jasig.cas.services.RegisteredServiceImpl;
+
+/**
+ * An extension to the registered service interface to allow a service to have a
+ * passcode. This is to provide additional service authentication during CASShib
+ * requests.
+ * 
+ * @author Brian Koehmstedt
+ */
+public class CasShibRegisteredService extends RegisteredServiceImpl {
+    /**
+     * Unique Id for serialization.
+     */
+    private static final long serialVersionUID = 4488281655556155879L;
+
+    private String passcode;
+
+    public String getPasscode() {
+        return passcode;
+    }
+
+    public void setPasscode(String passcode) {
+        this.passcode = passcode;
+    }
+
+    public boolean equals(final Object obj) {
+        boolean superEquals = super.equals(obj);
+        if (!superEquals)
+            return false;
+
+        if (!(obj instanceof CasShibRegisteredService))
+            return false;
+
+        final CasShibRegisteredService other = (CasShibRegisteredService) obj;
+
+        if (this.passcode == other.getPasscode()
+            || (this.passcode != null && other.getPasscode() != null && this.passcode
+                .equals(other.getPasscode())))
+            return true;
+        else
+            return false;
+    }
+}
